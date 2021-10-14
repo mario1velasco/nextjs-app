2e31b593d4b52d3695e239144a47268c9ae1e23f
Mario Velasco Alonso <mario.velasco@bizaway.com>
2021-10-14 16:57:03 +0200

    Fix prisma bug

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,9 @@
 // lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient;
+declare global {
+    var prisma: PrismaClient;
+}
 
 if (process.env.NODE_ENV === 'production') {
     prisma = new PrismaClient();
